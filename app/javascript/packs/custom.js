d08c1808a97fd40c63e01f8478ba77907324ab57
Ismael Sane <six.echo.sa@gmail.com>
2020-03-05 20:53:00 +0100

    Multiply inputs with JavaScript & display text box for the walls

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -42,7 +42,6 @@ details.oninput = function() {
   document.getElementById('walls_detail').style.display ='block';
   document.getElementById('todel').style.display ='block';
 }
-
 $(document).ready(function() {
     var max_fields = 10;
     var wrapper = $(".input_fields_wrap");
@@ -53,7 +52,11 @@ $(document).ready(function() {
         e.preventDefault();
         if (x < max_fields) {
             x++;
-            $(wrapper).append('<div><input type="hidden" id="monid" value="2"><select class="piece" name="piece[]"><option value="1">Séjour</option><option value="2">Cuisine</option><option value="3">Chambre1</option><option value="4">Chambre2</option><option value="5">Chambre3</option><option value="6">Chambre4</option><option value="7">WC</option><option value="8">SDB1</option><option value="9">SDB2</option><option value="10">Entrée</option><option value="11">Couloir</option><option value="12">Buanderie</option><option value="13">Garage</option><option value="14">Sous-sol</option><option value="15">Autres</option>                         </select>                         <select class="peinture" name="peinture[]"><option value="2:9.5">Murs Dec. Impression</option><option value="3:13.5">Murs Dec. Satiné Blanc</option><option value="4:15.2">Murs Dec. Satiné Pastel</option>                         </select>                         <input type="number" id="form_wall" name="wall_m[]" class="calcwall validate[required,custom[integer],min[1]] text-input" value="0" min="1" step="any" data-hasqtip="12">m²                         <input type="hidden" id="form_wall_coef" name="wall_coef[]" tabindex="-1" readonly="" value="2.62" title="Le coefficient 2.62 est un standard pour évaluer la surface des murs par rapport au sol">* coef Murs *                         <input type="text" id="form_wall_price" name="wall_price[]" tabindex="-1" readonly="" value="9.5">€ =                         <input type="text" id="form_wall_tot" name="wall_tot[]" tabindex="-1" readonly="" value="0">Total&nbsp;&nbsp;&nbsp;<br><a href="#" class="delete">Suprimer</a></div>'); //add input box
+            $(wrapper).append('<div><input type="hidden" id="monid" value="2"><select class="piece" name="piece[]"><option value="1">Séjour</option><option value="2">Cuisine</option><option value="3">Chambre1</option><option value="4">Chambre2</option><option value="5">Chambre3</option><option value="6">Chambre4</option><option value="7">WC</option><option value="8">SDB1</option><option value="9">SDB2</option><option value="10">Entrée</option><option value="11">Couloir</option><option value="12">Buanderie</option><option value="13">Garage</option><option value="14">Sous-sol</option><option value="15">Autres</option>                         </select>                         <select id="fw3" class="peinture" name="peinture[]"><option class="impress" value="9.5">Murs Dec. Impression</option><option class="satineBlanc" value="13.5">Murs Dec. Satiné Blanc</option><option class="satinePastel" value="15.2">Murs Dec. Satiné Pastel</option>                         </select>                         <input type="number" id="form_wall" name="wall_m[]" class="calcwall validate[required,custom[integer],min[1]] text-input" value="0" min="1" step="any" data-hasqtip="12">m²                         <input type="hidden" id="form_wall_coef" name="wall_coef[]" tabindex="-1" readonly="" value="2.62" title="Le coefficient 2.62 est un standard pour évaluer la surface des murs par rapport au sol">* coef Murs *                         <input type="text" id="form_wall_price" class="coefMurs3" name="wall_price[]" tabindex="-1" readonly="" value="9.5">€ =                         <input type="text" id="form_wall_tot" name="wall_tot[]" tabindex="-1" readonly="" value="0">Total&nbsp;&nbsp;&nbsp;<br><a href="#" class="delete">Suprimer</a></div>'); //add input box
+            let impression3 = document.getElementById("fw3");
+            impression3.onclick = function() {
+              document.querySelector('.coefMurs3').value = impression3.value;
+            }
         } else {
             alert('Vous avez atteint la limite de pièces à ajouter')
         }
@@ -65,3 +68,13 @@ $(document).ready(function() {
         x--;
     })
 });
+
+let impression = document.getElementById("fw");
+impression.onclick = function() {
+  document.querySelector('.coefMurs').value = impression.value;
+}
+let impression2 = document.getElementById("fw2");
+impression2.onclick = function() {
+  document.querySelector('.coefMurs2').value = impression2.value;
+}
+
