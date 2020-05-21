09268d7db519d39d3cc268759ebb7f08bbd3da37
Ismael Sane <six.echo.sa@gmail.com>
2020-05-21 17:45:53 +0200

    Add CRUD on quotes

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,56 @@
+class QuotesController < ApplicationController
+
+  def index
+    @quotes = Quote.all
+  end
+  def show
+    @quote = Quote.find(params[:id])
+    @devis_page = true
+
+    @totalht = @quote.totalht
+    @totalttc = @quote.totalttc
+    @tva = (@totalttc.to_i*10 - @totalht.to_i*10) / 10;
+    @ceiling_m = params[:ceiling_m]
+    @ceiling_total = params[:ceiling_total]
+    @city_chantier = params[:city2]
+    @walls = @quote.form_peinture
+    case @walls
+      when '9.5'
+        @roomDetails = "Murs Impression";
+      when '12'
+        @roomDetails = "Murs Blanc Mat";
+      when '14'
+        @roomDetails = "Murs Blanc Velour";
+      when '16'
+        @roomDetails = "Murs Pastel";
+      else
+        "Error"
+    end
+    @wallsquant = params[:walls_m]
+    @wallscoef = params[:walls_price]
+    @wallstot = params[:walls_tot]
+  end
+  def new
+    @quote = Quote.new
+  end
+  def create
+    quote = Quote.create(quote_params)
+    redirect_to quote_path(quote)
+  end
+  def edit
+
+  end
+  def update
+
+  end
+  def delete
+
+  end
+    private
+
+    def quote_params
+      # params.require(:quote).permit(:devis, :client, :adresse_site, :date,)
+
+      params.require(:quote).permit(:form_gender, :firstname, :lastname, :adresse, :zipcode, :city, :email, :adresse2, :zipcode2, :city2, :phone, :ceiling_m, :ceiling_price, :ceiling_total, :forfait1e, :check_walls, :form_peinture, :walls_m, :walls_coef, :walls_price, :walls_tot, :walls_total, :services_total, :totalht, :totalttc)
+    end
+end
--- db/migrate/20200509012417_create_quotes.rb
+++ db/migrate/20200509012417_create_quotes.rb
@@ -5,6 +5,32 @@ class CreateQuotes < ActiveRecord::Migration[6.0]
       t.string :client
       t.string :adresse_site
       t.date :date
+      t.string :form_gender
+      t.string :firstname
+      t.string :lastname
+      t.string :adresse
+      t.string :zipcode
+      t.string :city
+      t.string :email
+      t.string :adresse2
+      t.string :zipcode2
+      t.string :city2
+      t.string :phone
+      t.string :ceiling_m
+      t.string :ceiling_price
+      t.string :ceiling_total
+      t.string :forfait1e
+      t.string :check_walls
+      t.string :form_peinture
+      t.string :walls_m
+      t.string :walls_coef
+      t.string :walls_price
+      t.string :walls_tot
+      t.string :walls_total
+      t.string :services_total
+      t.string :totalht
+      t.string :totalttc
+
 
       t.timestamps
     end
