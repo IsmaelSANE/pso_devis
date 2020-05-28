92eb72733490106fbaa3efe2155012e225f21044
Ismael Sane <six.echo.sa@gmail.com>
2020-05-28 21:19:11 +0200

    Clean up the code

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'quotes#new'
+  resources :quotes
   get 'static_pages/home'
   # get 'static_pages/help'
   # sets :controller, :action and :id in params
   match 'static_pages/help', via: [:get, :post]
   get  'static_pages/about'
   get 'about'  => 'quotes#about'
-  resources :quotes
 end
