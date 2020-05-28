169db082a18a4b6114ee8339ffbebd32d80380bb
Ismael Sane <six.echo.sa@gmail.com>
2020-05-28 20:51:24 +0200

    Put quotes#new in the root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root 'quotes#new'
   get 'static_pages/home'
   # get 'static_pages/help'
   # sets :controller, :action and :id in params
